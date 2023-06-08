b9ebd7636bb1bd1048b5bf4dfa405916980b92fc
SUMIT SHARMA <211.sumit@gmail.com>
2023-06-08 14:31:19 +0530

    toggle status

--- todo-react/src/components/tasks/TaskList.tsx
+++ todo-react/src/components/tasks/TaskList.tsx
@@ -17,9 +17,9 @@ const TaskList = () => {
         fetchTaskList()
     }, []);
 
-    function deleteTask(id: string) {
+    const deleteTask = (id: string) => {
         if (window.confirm("Delete the Task?")) {
-            fetch(`http://localhost:5000/api/task/{id}`, {
+            fetch(`http://localhost:5000/api/task/${id}`, {
                 method: 'DELETE',
                 headers: {
                     'Content-Type': 'application/json'
@@ -29,6 +29,26 @@ const TaskList = () => {
                 .then(res => fetchTaskList());
         }
     }
+
+    const toggleStatus = (id: string, status: boolean) => {
+        fetch(`http://localhost:5000/api/task/${id}`, {
+            method: 'put',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 'status': status })
+        })
+            .then(response => response.json())
+            .then(res => fetchTaskList());
+    }
+    const completeTask = (id: string) => {
+        toggleStatus(id, false);
+    }
+
+    const openTask = (id: string) => {
+        toggleStatus(id, true);
+    }
+
 
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,6 +60,9 @@ const TaskList = () => {
 
     const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        if (!form.task_name) {
+            return false;
+        }
         const Tasks = {
             "name": form.task_name,
             "status": false
@@ -85,20 +108,32 @@ const TaskList = () => {
                     <Card.Body key={index} >
                         <Card bg="success" key={task._id}>
                             <div className="p-2 text-capitalize ">
-                                <span className="float-start">
-                                    <b>{ index+1 }</b> {task.name}
-                                </span>
-                                <span className="float-end">
-                                    <Row>
+                                <Row>
+                                    <Col>
+                                        <span className={`float-start ${(task.status) ? '' : ' text-decoration-line-through'}`}>
+                                            <b>{index + 1}</b>  {task.name + ' ' + task.status}
+                                        </span>
+                                    </Col>
+                                    <Col>
 
-                                        <Col>
-                                            <Button variant="primary outline-secondary" size="sm" >Complete</Button>
-                                        </Col>
-                                        <Col>
-                                            <Button key={task._id} variant="danger outline-secondary" size="sm" onClick={ ()=> deleteTask(task._id)} >Delete</Button>
-                                        </Col>
-                                    </Row>
-                                </span>
+                                        <span className="float-end">
+                                            <Row>
+
+                                                <Col>
+                                                    {(task.status) && <Button variant="primary outline-secondary" size="sm" onClick={() => completeTask(task._id)}>Complete</Button>}
+                                                    {(!task.status) && <Button variant="primary outline-secondary" size="sm" onClick={() => openTask(task._id)} >Re-Open</Button>}
+
+
+                                                    {/* <Button variant="primary outline-secondary" size="sm" onClick={() => completeTask(task._id)} >Complete</Button> */}
+                                                </Col>
+                                                <Col>
+                                                    <Button key={task._id} variant="danger outline-secondary" size="sm" onClick={() => deleteTask(task._id)} >Delete</Button>
+                                                </Col>
+                                            </Row>
+                                        </span>
+                                    </Col>
+                                </Row>
+
                             </div>
                         </Card>
                     </Card.Body>
